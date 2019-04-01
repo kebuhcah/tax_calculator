4079ced2a42efc37271256bc7943bbd0bc7aa73b
Kevin Sun <kevsun1@gmail.com>
2019-04-01 16:43:53 -0400

    stack for small screens

--- calculator.js
+++ calculator.js
@@ -128,7 +128,7 @@ class TaxCalculation extends React.Component {
                 className: "row"
             },
             el("div", {
-                    className: "col col-6"
+                    className: "col col-md-6 col-sm-12 col-12"
                 },
                 el("div", {
                         className: "card"
@@ -164,7 +164,7 @@ class TaxCalculation extends React.Component {
                         className: "card-footer"
                     }, "Total: " + dollar(oldState + oldCityVal + oldMansionVal)))),
             el("div", {
-                    className: "col col-6"
+                    className: "col col-md-6 col-sm-12 col-12"
                 },
                 el("div", {
                         className: "card"
